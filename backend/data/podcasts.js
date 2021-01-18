e7357d03e0f3b2c7876b9822b30ce1292a2da66c
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-01-18 16:13:44 +0000

    added sample users

--- backend/data/podcasts.js
+++ backend/data/podcasts.js
@@ -1,34 +1,34 @@
 const podcasts = [
   {
-    _id:  '1',
+    
     name: 'Frank Quitely',
     image: '/images/superman.jpg',
     time: '',
     description: 'Does a podcast with Frank Quitely need a description? Its Frank Quitely. Hes one of the most revered, accomplished comic book artists in the history of the medium. His ink has graced The New X-Men, All-Star Superman, Batman and Robin, The Authority, and countless other comics, in addition to laying the foundation for books including Jupiters Legacy and We3. Please enjoy.',
   },
   {
-    _id:  '2',
+    
     name: 'Nicola Streeton',
     image: '/images/streeton.jpg',
     time: '',
     description: 'Nicola is an anthropologist-turned-illustrator and comics scholar. She is the author of Billy, Me & You (Myriad, 2011), an acclaimed graphic memoir about her bereavement following the death of her two-year old son. It was initially serialised in Liquorice Magazine, a comic Nicola produced with her daughter, and it received Highly Commended in the Popular Medicine category of the 2012 British Medical Association Medical Book Awards. Nicola is also co-editor with Cath Tate of The Inking Woman, published by Myriad in March 2018.',
   },
   {
-    _id:  '3',
+    
     name: 'David Bishop',
     image: '/images/2000.jpg',
     time: '',
     description: '',
   },
   {
-    _id:  '4',
+    
     name: 'Jim Alexander',
     image: '/images/jimbot.jpg',
     time: '',
     description: '',
   },
   {
-    _id:  '5',
+    
     name: 'Leah Moore',
     image: '/images/ravens.jpg',
     time: '',
---
+++ backend/data/users.js
@@ -0,0 +1,19 @@
+import bcrypt from 'bcryptjs'
+
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true
+  },
+  {
+    name: 'Tim',
+    email: 'tim@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    
+  }
+]
+
+export default users
