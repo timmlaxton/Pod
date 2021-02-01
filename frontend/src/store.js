0c5e33d74037614d224fe6c821fe2311f50c5524
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-02-01 17:41:31 +0000

    added login page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,19 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { podcastListReducer, podcastDetailsReducer } from './reducers/podcastReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
 	podcastList: podcastListReducer,
-	podcastDetails: podcastDetailsReducer
+	podcastDetails: podcastDetailsReducer,
+	userLogin: userLoginReducer
 });
 
-const initialState = {};
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+
+const initialState = {
+	userLogin: { userInfo: userInfoFromStorage }
+};
 
 const middleware = [thunk];
 
